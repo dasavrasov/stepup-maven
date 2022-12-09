be1dddd6b3832dc2dbf553c9afae45c7fd02da6f
DenisS <d_savrasov@rambler.ru>
2022-12-09 16:56:07 +0300

    #1 Инкапсуляция

--- src/main/java/ru/stepup/payments/mobile/MainApplication.java
+++ src/main/java/ru/stepup/payments/mobile/MainApplication.java
@@ -7,28 +7,12 @@ import java.util.Scanner;
 
 public class MainApplication {
     public static void main(String[] args) {
-        // Создать Ломаную, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}
-        Polyline polyline=new Polyline(Arrays.asList(new Point(1,5),new Point(2,8),new Point(5,3),new Point(8,9)));
-        System.out.println(polyline);
-        // Рассчитать длину Ломаной
-        double polylen=polyline.len();
-        System.out.println("Длина ломаной: "+polylen);
-        //Получить у Ломаной массив Линий
-        List<Line> lines=polyline.getLines();
-        //Рассчитать длину массива Линий
-        double length=0;
-        for (Line line: lines) {
-            length+=line.len();
-        }
-        System.out.println("Длина массива линий: "+length);
+        //f1 это одна треть, f2 две пятых, а f3 это семь восьмых
+        Fraction f1=Fraction.of(1,3);
+        Fraction f2=Fraction.of(2,5);
+        Fraction f3=Fraction.of(7,8);
 
-        //Сравнить длину Ломаной и массива Линий: они должны совпасть
-        System.out.println("Результат сравнения длины массива линий и длины ломаной: ");
-        System.out.println(length== polylen);
-        //Сдвинуть координату Точки {2,8} таким образом, чтобы она стала иметь значение {12,8}.
-        // Если изменения отразились в данной точке, в Ломаной и в двух Линиях массива (из пункта 3), то задача решена верно
-        polyline.points.get(1).x=12;
-        System.out.println(polyline);
-
+        //Посчитать f1.sum(f2).sum(f3).minus(5)
+        System.out.println("Результат: "+f1.sum(f2).sum(f3).minus(5));
     }
 }
