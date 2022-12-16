92c31ea4628c1dac676c46f2062dfcc7f2b4ede2
DenisS <d_savrasov@rambler.ru>
2022-12-16 19:15:54 +0300

    #3 Пакеты Степень

--- src/main/java/ru/courses/main/MainApplication.java
+++ src/main/java/ru/courses/main/MainApplication.java
@@ -1,7 +1,15 @@
 package ru.courses.main;
 
+import java.util.Scanner;
+import static java.lang.Math.*;
+import static java.lang.Integer.*;
+
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Результат: "+pow2(args[0],args[1]));
+    }
+
+    public static double pow2(String x, String y){
+        return pow(parseInt(x),parseInt(y));
     }
 }
