0f3ec851b2b50915119b5539a16a7229ad2cdaa9
DenisS <d_savrasov@rambler.ru>
2023-01-26 00:52:55 +0300

    Рефлексия 1

--- src/main/java/ru/stepup/payments/mobile/Sum.java
+++ src/main/java/ru/stepup/payments/mobile/Sum.java
@@ -1,46 +1,36 @@
 package ru.stepup.payments.mobile;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Sum {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IllegalAccessException {
+        Cat cat = new Cat("Vasya", 10, new ArrayList<>(Arrays.asList("Anton", "Oleg", "Igor")));
+
+        System.out.println(cat);
+        nullAllFields(cat);
+        System.out.println(cat);
+
+        Dog dog = new Dog("Sharik",5);
+
+        System.out.println(dog);
+        nullAllFields(dog);
+        System.out.println(dog);
+
+
     }
 
-    public static void reverse(ArrayList<Integer> intList) {
-        int n = intList.size() - 1;
-        for (int i = 0; i < intList.size() / 2; i++) {
-            int temp = intList.get(i);
-            intList.set(i,intList.get(n - i));
-            intList.set(n - i,temp);
+    static void nullAllFields(Object obj) throws IllegalAccessException{
+        for (Field field: obj.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            if (field.getType().getName().equals("int") || field.getType().getName().equals("long") || field.getType().getName().equals("double"))
+                field.set(obj,0);
+            else
+                field.set(obj,null);
         }
-    }
-    public static void bubbleSort(int[] intArray) {
-        for (int i = 0; i < intArray.length - 1; i++) {
-            for (int j = 0; j < intArray.length - i - 1; j++) {
-                if (intArray[j] > intArray[j + 1]) {
-                    int temp = intArray[j];
-                    intArray[j]=intArray[j + 1];
-                    intArray[j + 1]= temp;
-                }
-            }
-        }
-    }
-
-    public static void task1() {
-        int N=20;
-        int j;
-        List<Integer> list=new ArrayList<>();
-
-        for(int i=0;i<N;i++)
-            list.add(i);
-
-        for(int i=0;i<N;i+=2){
-            j=list.get(i);
-            list.set(i, list.get(i+1));
-            list.set(i+1,j);
-        }
-
-        System.out.println(list);
     }
 }
